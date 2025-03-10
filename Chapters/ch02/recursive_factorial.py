30b6e165d67da9f8873bfb24a3a504c265e43525
Proloy Mishra <namitapro0987@gmail.com>
2025-03-10 12:10:13 +0530

    setup

--- Chapters/ch02/recursive_factorial.py
+++
@@ -1,9 +0,0 @@
-def factorial(n):
-    if n == 0:
-        return 1
-    else:
-        return n * factorial(n-1)
-
-# Example usage
-n = 5
-print(f"Factorial of {n} is {factorial(n)}")
--- Chapters/ch03/eigenvalues.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-
-A = np.array([[4, 1], [2, 3]])
-eigenvalues, eigenvectors = np.linalg.eig(A)
-print("Eigenvalues:\n", eigenvalues)
-print("Eigenvectors:\n", eigenvectors)
